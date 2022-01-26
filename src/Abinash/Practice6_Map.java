2737c2a5fc413477a2784bd8489754e361208192
Abinash <abinashpatra123.com>
2022-01-26 14:10:35 +0530

    Hashmap Implementation

---
+++ src/Abinash/Practice6_Map.java
@@ -0,0 +1,16 @@
+package Abinash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Practice6_Map {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String,String> mapper=new HashMap<String,String>();
+		mapper.put("Prerna", "Mohanty");
+		mapper.put("Abinash", "Patra");
+
+	}
+
+}
